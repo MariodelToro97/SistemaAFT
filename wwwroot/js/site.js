b70afcf138f89b415e124d5f3c8ca9f21474549c
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2020-03-08 16:02:51 -0600

    JavaScript aumentado

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,49 +1,51 @@
-﻿$(document).ready(function () {
-
-});
-
-function errorCurp(campo) {
+﻿$(document).ready( function() {
+    $('#personaFisica').click(function () {
+        $('#nombreMoral').hide();
+        $('#curpHistoricas').show();
+        $('#nav-representantes-tab').hide();
+        $('#nav-integrantes-tab').hide();
+        $('#columnaDos').show();
+        $('#divCURP').show();
+        $('#imagenSearch').show();
+        $('#imagenSearchRFC').hide();
+        $('#tableCurpHist').hide();
+        
+        document.getElementById('nombreGeneral').required = true;
+        document.getElementById('nombreGeneral').setPointerCapture = "Campo requerido";
+        document.getElementById('apellidoPaterno').required = true;
+        document.getElementById('rfcPersona').required = false;
     
-    campo.setCustomValidity("Campo Requerido");
-}
-
-$('#personaFisica').click(function () {
-    $('#nombreMoral').hide();
-    $('#curpHistoricas').show();
-    $('#nav-representantes-tab').hide();
-    $('#nav-integrantes-tab').hide();
-    $('#columnaDos').show();
-    $('#divCURP').show();
-    $('#imagenSearch').show();
-    $('#imagenSearchRFC').hide();
+        document.getElementById('labelRFC').innerHTML = 'RFC:';
+        document.getElementById('spanRFC').innerHTML = "";
+    }); 
     
-    document.getElementById('nombreGeneral').required = true;
-    document.getElementById('apellidoPaterno').required = true;
-    document.getElementById('rfcPersona').required = false;
-});
-
-
-$('#personaMoral').click(function () {
-    $('#nombreMoral').show();
-    $('#curpHistoricas').hide();
-    $('#nav-representantes-tab').show();
-    $('#nav-integrantes-tab').show();
-    $('#columnaDos').hide();
-    $('#divCURP').hide();
-    $('#imagenSearch').hide();
-    $('#imagenSearchRFC').show();
-
-    document.getElementById('nombreGeneral').required = false;
-    document.getElementById('apellidoPaterno').required = false;
-    document.getElementById('rfcPersona').required = true;
-});
-
-$('#form input[type=text]').on('change invalid', function () {
-    var campotexto = $(this).get(0);
-
-    campotexto.setCustomValidity('');
-
-    if (!campotexto.validity.valid) {
-        campotexto.setCustomValidity('Campo requerido');
+    if(document.getElementById('spanCURP').innerHTML = "This field is required."){
+        document.getElementById('spanCURP').innerHTML = "Campo requerido";
     }
+    
+    $('#personaMoral').click(function () {
+        $('#nombreMoral').show();
+        $('#curpHistoricas').hide();
+        $('#nav-representantes-tab').show();
+        $('#nav-integrantes-tab').show();
+        $('#columnaDos').hide();
+        $('#divCURP').hide();
+        $('#imagenSearch').hide();
+        $('#imagenSearchRFC').show();
+        $('#tableCurpHist').show();
+    
+        document.getElementById('nombreGeneral').required = false;
+        document.getElementById('apellidoPaterno').required = false;
+        document.getElementById('rfcPersona').required = true;
+    
+        document.getElementById('labelRFC').innerHTML = 'RFC<span class="text-danger font-weight-bold">*</span>:';
+    });
+
+    $('#CURP_Historicas').click(function () {
+        if ($('#tableCurpHist').is(":visible")) {
+            $('#tableCurpHist').hide();
+        } else {
+            $('#tableCurpHist').show();
+        }
+    });
 });
